044f7706bf56dd953ae1e3ca92d3783bdc4393de
Malek-Abdelal <malekbassam1144@gmail.com>
2023-05-05 20:38:29 +0300

    Adding lucas function and it's tests

--- series/series.py
+++ series/series.py
@@ -13,6 +13,20 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
 
 
+def lucas(n):
+    '''  this function calculates the lucas for a number.
+    :param => n:int 
+    :return => int 
+    '''
+
+    if n == 0 :
+        return 2 
+    if n == 1 :
+        return 1
+    if n > 1 :
+        return lucas(n-1) + lucas(n-2)
+
+
 
     
     
--- tests/test_series.py
+++ tests/test_series.py
@@ -32,4 +32,30 @@ def test_fibonacci_9():
     expected = 34
     assert actual == expected
 
+#lucas tests
+def test_lucas_0():
+    actual = series.series.lucas(0)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_1():
+    actual = series.series.lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_2():
+    actual = series.series.lucas(2)
+    expected = 3
+    assert actual == expected
+
+def test_lucas_6():
+    actual = series.series.lucas(6)
+    expected = 18
+    assert actual == expected
+
+def test_lucas_11():
+    actual = series.series.lucas(11)
+    expected = 199
+    assert actual == expected
+    
 
